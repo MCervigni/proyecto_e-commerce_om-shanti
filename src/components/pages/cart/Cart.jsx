0d5b6339ad59315db02aa6a566999fe67d86ac4c
Marina Cervigni <name>
2024-11-18 22:57:13 -0300

    NavegaLasRutas+Cervigni

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -4,7 +4,7 @@ const Cart = () => {
   return (
     <div>
       <h2 style={{ fontStyle: "italic", fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>Página del Carrito</h2>
-      <h3 style={{ fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>⚠ En contrucción ⚠</h3>
+      <h3 style={{ fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>⚠ En construcción ⚠</h3>
       <Link to="/checkout">Finalizar compra</Link>
     </div>
   );
--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,8 +1,8 @@
 const Checkout = () => {
   return (
     <div>
-      <h2 style={{ fontStyle: "italic", fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>Página de Finalizar Compra</h2>
-      <h3 style={{ fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>⚠ En contrucción ⚠</h3>
+      <h2 style={{ fontStyle: "italic", fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>Página Finalizar Compra</h2>
+      <h3 style={{ fontSize: "1.4rem" , fontWeight:500, color:"gray"}}>⚠ En construcción ⚠</h3>
     </div>
   );
 };
